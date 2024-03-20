7cd109852196a6dda55d782c5900460dc77c7ea4
martin <tinbonino@gmail.com>
2024-03-19 22:13:28 -0300

    Backend Clase 01

---
+++ Backend/script.js
@@ -0,0 +1,22 @@
+const numeroEntero=32;
+const numeroDecimal=3.14;
+const cadenaDeTexto="Aguante el back 💪";
+const booleano=true;
+const array=["Carlos","Agus","Cande","Santino","Claudio"];
+const objeto={
+    nombre:"Atxu",
+    profesion:"programadora"
+}
+const funcion= function(){
+    console.log("Soy una función");
+}
+
+console.log("Numero entero: ",numeroEntero);
+console.log("Numero decimal: ",numeroDecimal);
+console.log("String: ",cadenaDeTexto);
+console.log("Booleano ",booleano);
+console.log("Array: ",array);
+console.log("objeto ",objeto);
+funcion();
+
+
